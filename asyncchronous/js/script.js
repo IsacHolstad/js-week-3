f5250c567d72ace42cd74af3a3c4575dee17920b
IsacHolstad <wisac22@gmail.com>
2022-09-21 13:17:32 +0200

    using filter method

--- asyncchronous/js/script.js
+++ asyncchronous/js/script.js
@@ -15,12 +15,58 @@ console.log(a + b);
 // asyncronic code
 
 console.log("I");
-setTimeout(()=>{
-    console.log("eat")
-
-}, 3000);
+//setTimeout(()=>{
+//    console.log("eat")
+//
+//}, 3000);
 
 console.log("ice cream");
+
+// Callbacks();
+
+// Example function that will get passed as a callback
+function exampleFunction() {
+    console.log('Example function called');
+}
+
+// Our main function that will run the callback function
+// being passed to it
+function doSomethingWithCallback(callbackFunction) {
+    // Calling the function that is being passed
+    callbackFunction();
+}
+
+// Pass "exampleFunction" function into our main
+// "doSomethingWithCallback" function
+doSomethingWithCallback(exampleFunction);
+
+
+// create a callback function
+//that takes function as an agrument
+// this callback function is who is eating ice cream
+
+//function whoIsEatingIceCream(callBackFunction) {
+   // callBackFunction();
+//}
+//function showName(){
+//    console.log(`i am ${name}  i am eating ice cream`);
+//}
+
+//whoIsEatingIceCream(showName());
+
+
+const values = [5, 32, 53, 20, 10, 9, 23];
+
+const newValues = values.filter((value) =>{
+    if (value > 10) {
+        return true
+    }
+    else {
+        return  false
+    }
+})
+console.log("new values: " ,newValues)
+
 
 
 
