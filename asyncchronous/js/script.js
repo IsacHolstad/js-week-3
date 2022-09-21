052e6374a85a7cd1ed58bd87ddfa604f036f758c
IsacHolstad <wisac22@gmail.com>
2022-09-21 13:47:50 +0200

    finally

--- asyncchronous/js/script.js
+++ asyncchronous/js/script.js
@@ -69,6 +69,48 @@ console.log("new values: " ,newValues);
 //console.log(newValues)
 
 
+//Promises
+
+const myPromise = new Promise((resolve, reject) => {
+    // We are hard coding a value for this example
+    const shouldResolve = true;
+    if (shouldResolve) {
+        // If the code inside of the promise was able to do what
+        // it needed to do, then it will call this 'resolve' callback
+        resolve('Promise resolved successfully.');
+    } else {
+        // If the code inside of the promise failed to do what
+        // it needed to do then it will call this 'reject' callback
+        reject('Promise has failed');
+    }
+});
+
+
+console.log(myPromise);
+
+// chaining();
+// We fetch the data from the URL
+let isLoading = true
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    // We have to asynchronously parse the data into JSON
+    //   so we can make use of it
+    .then((response) => response.json())
+    // Data is now available as
+    .then((json) => console.log(json))
+     //
+    .catch((error) => {
+        console.log('An error has been thrown');
+        console.log(error);
+
+    })
+    .finally(
+       isLoading = false
+    );
+
+
+
+
+
 
 
 
